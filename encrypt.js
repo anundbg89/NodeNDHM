d764c41c505e2b4c86cb6296917cd08793a310ac
anundbg89 <42108126+anundbg89@users.noreply.github.com>
2022-12-16 00:00:10 +0530

    Initial commit

---
+++ encrypt.js
@@ -0,0 +1,32 @@
+const crypto = require('crypto')
+const NodeRSA = require('node-rsa')
+const path = require("path")
+const fs = require("fs")
+module.exports = {
+     encryptDataFunc : function encryptDataFunc(stringToEncrypt){
+        //const absolutePath = path.resolve('./public_key.pem')
+        const publicKey = `-----BEGIN PUBLIC KEY-----
+        MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAstWB95C5pHLXiYW59qyO
+        4Xb+59KYVm9Hywbo77qETZVAyc6VIsxU+UWhd/k/YtjZibCznB+HaXWX9TVTFs9N
+        wgv7LRGq5uLczpZQDrU7dnGkl/urRA8p0Jv/f8T0MZdFWQgks91uFffeBmJOb58u
+        68ZRxSYGMPe4hb9XXKDVsgoSJaRNYviH7RgAI2QhTCwLEiMqIaUX3p1SAc178ZlN
+        8qHXSSGXvhDR1GKM+y2DIyJqlzfik7lD14mDY/I4lcbftib8cv7llkybtjX1Aayf
+        Zp4XpmIXKWv8nRM488/jOAF81Bi13paKgpjQUUuwq9tb5Qd/DChytYgBTBTJFe7i
+        rDFCmTIcqPr8+IMB7tXA3YXPp3z605Z6cGoYxezUm2Nz2o6oUmarDUntDhq/PnkN
+        ergmSeSvS8gD9DHBuJkJWZweG3xOPXiKQAUBr92mdFhJGm6fitO5jsBxgpmulxpG
+        0oKDy9lAOLWSqK92JMcbMNHn4wRikdI9HSiXrrI7fLhJYTbyU3I4v5ESdEsayHXu
+        iwO/1C8y56egzKSw44GAtEpbAkTNEEfK5H5R0QnVBIXOvfeF4tzGvmkfOO6nNXU3
+        o/WAdOyV3xSQ9dqLY5MEL4sJCGY1iJBIAQ452s8v0ynJG5Yq+8hNhsCVnklCzAls
+        IzQpnSVDUVEzv17grVAw078CAwEAAQ==
+        -----END PUBLIC KEY-----`;
+    
+        var key = new NodeRSA();
+        key.importKey(publicKey, 'pkcs8-public');
+        key.setOptions({environment: 'node', encryptionScheme: 'pkcs1'});
+        const result = key.encrypt(stringToEncrypt, 'base64');
+        return result;
+    }
+}
+
+
+
---
+++ server.js
@@ -0,0 +1,87 @@
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+var bodyParser = require('body-parser')
+app.use(bodyParser.json({limit: '25mb'}));
+app.use(bodyParser.urlencoded({limit: '25mb', extended: true}));
+var sf = require('node-salesforce');
+const {encryptDataFunc,} = require("./encrypt.js");
+
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+console.log('port is'+ port);
+server.listen(port)
+
+var conn = new sf.Connection({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+   loginUrl : 'https://login.salesforce.com'
+});
+
+var conn1 = new sf.Connection({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+   loginUrl : 'https://login.salesforce.com'
+});
+var username = 'anuj.singh2@resourceful-bear-nxv21i.com'
+var password = 'Anuj1990J0IJpV8tW41sKfstnLOC6e6NH'
+
+var AbhijtdaUsername  = 'sudiptahalder@gmail.com.tcrm';
+var AbhijtdaPassword  = 'sudiptada123ed946uJXVfcXfbiBqlV2gA9FU';
+
+conn1.login(AbhijtdaUsername, AbhijtdaPassword, function(err, userInfo) {
+  if (err) { return console.error(err); }
+  // Now you can get the access token and instance URL information.
+  // Save them to establish connection next time.
+  console.log(conn.accessToken);
+  console.log(conn.instanceUrl);
+  // logged in user property
+  console.log("User ID: " + userInfo.id);
+  console.log("Org ID: " + userInfo.organizationId);
+  // ...
+});
+
+conn.login(username, password, function(err, userInfo) {
+  if (err) { return console.error(err); }
+  // Now you can get the access token and instance URL information.
+  // Save them to establish connection next time.
+  console.log(conn.accessToken);
+  console.log(conn.instanceUrl);
+  // logged in user property
+  console.log("User ID: " + userInfo.id);
+  console.log("Org ID: " + userInfo.organizationId);
+  // ...
+});
+
+app.use(function (req, res, next) {
+  var responseObject;
+  const reqpath = req.path;
+  if(reqpath.includes('Salesforce/NDHMEncrypt')){ 
+    console.log('Data to check' +req.body.dataToEncrypt)
+    encryptDataFunc(req.body.dataToEncrypt)
+    res.end(encryptDataFunc(req.body.dataToEncrypt)); 
+  }
+  else{
+    responseObject = req.body;
+    responseObject["inboundUrl"] = req.path;
+    console.log('path'+ reqpath);
+    console.log('final body'+JSON.stringify(req.body));
+    
+    //responseObject.inboundUrl = req.path;
+    console.log('Final Data' + responseObject);
+    conn.apex.post("/gateway/onFetchMode/", responseObject, function(res) {
+    // the response object structure depends on the definition of apex class
+    console.log('request sent back')
+    });
+    conn1.apex.post("/gateway/onFetchMode/", responseObject, function(res) {
+      // the response object structure depends on the definition of apex class
+      console.log('request sent back')
+      });
+  }
+  
+  next()
+})
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  console.log('addr: ' + add);
+})
